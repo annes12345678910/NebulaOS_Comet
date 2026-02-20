670318dac4e5c4e168576db8858fa50f98074b46
annes <anneswidow@gmail.com>
2026-02-20 15:26:23 +0300

    cursor

---
+++ cursor.py
@@ -0,0 +1,22 @@
+import os
+import load
+import renderer,ultimateraylib as rl
+
+cursors = []
+current = 0
+'''
+0 = arrow
+1 = pointing
+2 = resize
+'''
+
+def draw():
+    rl.draw_texture_ex(cursors[current], rl.Vector2(*renderer.get_mouse_pos()), 0, 0.45 if renderer.is_mouse_left_down() else 0.5, rl.WHITE)
+
+def init():
+    global cursors
+    cursors = {}
+    for i in os.listdir(str(load.fold / 'assets/cursor')):
+        if os.path.isfile((load.fold / f'assets/cursor/{i}')):
+            cursors[(load.fold / f'cursor/{i}').stem] = load.load_sound(f"assets/sound/{i}")
+
